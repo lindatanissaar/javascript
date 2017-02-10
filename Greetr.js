0e37c67e780b1315bdbce13a60bb1f87fbec2d98
Linnu <linda.tanissaar@khk.ee>
2017-02-10 17:34:39 +0200

    J2_StructuringSafeCode

---
+++ Greetr.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Linnu on 10.02.2017.
+ */
+//luuakse IIFE funktsioon, millel on kaks parameetrit (global, $)
+// parameetritele antakse väärtused window ja jQUery
+(function(global, $) {
+
+}(window, jQuery));
