33b6725305d6b9d6a3df0b8cdba1be3fa146c0bb
Linnu <linda.tanissaar@khk.ee>
2017-02-10 18:37:26 +0200

    J7_LetsUseOurFramework

--- Greetr.js
+++ Greetr.js
@@ -3,7 +3,7 @@
  */
 //luuakse IIFE funktsioon, millel on kaks parameetrit (global, $)
 // parameetritele antakse väärtused window ja jQUery
-//viimases polnud muutusi
+//viimases polnud muutuseid
 (function(global, $) {
     //luuakse objekt, selleks et ei peaks kasutama keyword new'd kogu aeg
     var Greetr = function(firstName, lastName, language) {
