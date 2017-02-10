970d7bf894a5db7d8f77f8f23e86d34acc4990f2
Linnu <linda.tanissaar@khk.ee>
2017-02-10 18:18:05 +0200

    J3_OurObjectAndPrototype

--- Greetr.js
+++ Greetr.js
@@ -12,6 +12,94 @@
     //luuakse tühi objekti prototüüp
     Greetr.prototype = {};
 
+    //luuakse järjend keeltele (inglise keel ja hispaania keel)
+    var supportedLangs = ['en', 'es'];
+
+    //luuakse greetings objekt (nimi:väärtus paaridena)
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    //luuakse formalGreetings objekt (nimi:väärtus paaridena)
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    //luuakse logMessages objekt (nimi:väärtus paaridena)
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inició sesión'
+    };
+
+
+    //meetodid
+    Greetr.prototype = {
+
+        //luuakse fullName meetod, mis väljastab alati eesnime + perenime
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        //luuakse valideerimise meetod; vaatab, kas konkreetne keel on toetatud, kui mitte, väljastab errori
+        validate: function() {
+            if (supportedLangs.indexOf(this.language)  === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        //luuakse meetod greeting, mis tagastab konkreetsest keeles tervituse koos eesnime ja hüüumärgiga
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        //luuakse meetod formalGreeting, mis tagastab konkreetsest keeles tervituse koos täispika nimega
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+
+        greet: function(formal) {
+            var msg;
+
+            // if undefined or null it will be coerced to 'false'
+            if (formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+
+
+        //kui console objekt on olemas (true), kirjutab konsooli vastava sõnumi
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName());
+            }
+
+            return this;
+        },
+
+        //valideeritakse keel
+        setLang: function(lang) {
+            this.language = lang;
+
+            this.validate();
+
+            return this;
+        }
+
+    };
+
     //luuakse funktsioon, millel on kolm parameetrit(firstName, lastName, language)
     Greetr.init = function(firstName, lastName, language) {
 
@@ -31,7 +119,6 @@
     global.Greetr = global.G$ = Greetr;
 
 }(window, jQuery));
-
 
 
 
