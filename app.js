0f934e72665a4a3fb37ae9e35b2f5b2600acf0e1
Linnu <linda.tanissaar@khk.ee>
2017-02-10 19:03:34 +0200

    J7_LetsUseOurFramework

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ var g = G$('John', 'Doe');
 //kutsutakse meetodit greet ning keeleks määratakse hispaania keel
 g.greet().setLang('es').greet(true).log();
 
-//6. on sama
+
+// obkjekti kasutatakse pärast seda kui on sisse logitud
+$('#login').click(function() {
+
+    // loob uue Greetr objekti
+    var loginGrtr = G$('John', 'Doe');
+
+    // peidab login divi ekraanilt
+    $('#logindiv').hide();
+
+    // vastavalt keelele, saadetakse tervitus ja logitakse kasutaja sisse
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+
+});
